021df020628f86aec7e9dd42822b7c79b6f58fa5
demieane <danevlavi@mail.ntua.gr>
2022-05-11 16:28:26 +0300

    triangle ok

--- triangle.c
+++ triangle.c
@@ -2,9 +2,11 @@
 
 // initialize variables
 int i,j;
+//int height = 8;
+int height = 190;
 
 int main(){
-    for (i=1; i<=8; i++){
+    for (i=1; i<=height; i++){
         for (j=1; j<=i; j++){
             //printf("X: i=%d,j=%d ",i,j);
             printf("X");
